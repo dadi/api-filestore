1a77e012b57e52b882d68b35d7f7a6dd08c7947f
James Lambie <jameslambie@gmail.com>
2017-02-18 08:46:08 +0800

    feat: add post install script for config file copy
    Close #6

---
+++ scripts/copy-config.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var path = require('path')
+
+var currentPath = process.cwd() // should be node_modules/@dadi/api-filestore
+var configPath = path.join(__dirname, '../config/filestore.development.json.sample')
+var destinationDir = path.join(currentPath, '../../../config')
+var destinationFile = path.join(destinationDir, 'filestore.development.json')
+
+mkdirp(destinationDi, (err, made) => {
+  if (err) throw err
+
+  fs.readFile(configPath, (err, data) => {
+    if (err) throw err
+
+    fs.writeFile(destinationFile, data, (err) => {
+      if (err) throw err
+
+      console.log('Filestore configuration created at', destinationFile)
+    })
+  })
+}
